5461d5abd2248ead4332171dd5ed3e1179123216
gumbyferret <yb@converged-systems.com>
2015-12-23 20:40:13 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  })
-})
+  //console.log('a user connected');
+  //socket.on('disconnect', function(){
+  //  console.log('user disconnected');
+  //})
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
 
 http.listen(3000, function(){
   console.log('listening on 192.168.1.101:3000');
