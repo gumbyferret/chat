8e5045c69eaa318c1289d55a5a8c71b2fe5d9cb4
gumbyferret <yb@converged-systems.com>
2015-12-23 20:10:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res) {
   res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000m function(){
+http.listen(3000, function(){
   console.log('listening on 192.168.1.101:3000');
 });
