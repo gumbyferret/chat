aa460c8e2f9625399b4bbe2d7c14462b8e44ba7f
gumbyferret <yb@converged-systems.com>
2015-12-23 20:30:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
   //res.send('<h1>Hello World</h1>');  //send Hello World
   res.sendFile(__dirname + '/index.html'); //send a file
 });
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+})
 
 http.listen(3000, function(){
   console.log('listening on 192.168.1.101:3000');
