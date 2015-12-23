ed21d3f1cfb3df25eccbfbd0ebb38344981f0613
gumbyferret <yb@converged-systems.com>
2015-12-23 20:41:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ io.on('connection', function(socket) {
   //})
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
+    io.emit('chat message', msg); //broadcast message to everyone
   });
 });
 
